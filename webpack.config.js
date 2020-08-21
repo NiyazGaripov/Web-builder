14536fa2d5e9d96d6627ae05886969f7bca45e01
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-21 23:08:10 +0300

    Adds base config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,10 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+}
