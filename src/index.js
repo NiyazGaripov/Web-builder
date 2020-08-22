30cd9c36dad89d622d06cff8e9771022403c1d32
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-22 22:52:15 +0300

    Creates func createTitleMarkup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import {models} from './model';
 import './styles/main.css'
 
-console.log(models)
+const createTitleMarkup = (block) => {
+    return (
+        `<div class="row">
+            <div class="col-sm">
+                <h1>${block.value}</h1>
+            </div>
+        </div>`
+    )
+}
