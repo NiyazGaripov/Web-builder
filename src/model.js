4e1aef410e4daa7a879a8a3bb93915f9c1d5a074
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-22 22:43:57 +0300

    Creates model

---
+++ src/model.js
@@ -0,0 +1,5 @@
+export const models = [
+    {type: `title`, value: `Web Builder`, options: {}},
+    {type: `text`, value: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur error expedita hic itaque maiores minus nulla numquam perferendis placeat sapiente!`, options: {}},
+    {type: `columns`, value: [`first column`, `second column`, `third column`], options: {}},
+];
