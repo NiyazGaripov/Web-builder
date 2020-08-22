d34e540331198ae79a910bebeb50b2123f6282bf
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-22 23:22:18 +0300

    Iterates over model and renders desired block

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const createTitleMarkup = (block) => {
                 <h1>${block.value}</h1>
             </div>
         </div>`
-    )
+    );
 };
 
 const createTextMarkup = (block) => {
@@ -18,15 +18,35 @@ const createTextMarkup = (block) => {
                 <p>${block.value}</p>
             </div>
         </div>`
-    )
+    );
 };
 
 const createColumnsMarkup = (block) => {
-    const column = block.value.map(item => `<div class="col-sm"><p>${item}</p></div>`)
+    const column = block.value.map(item => `<div class="col-sm"><p>${item}</p></div>`).join('');
 
     return (
         `<div class="row">
             ${column}
         </div>`
-    )
+    );
 };
+
+models.forEach(block => {
+    let html;
+
+    switch (block.type) {
+        case "title":
+            html = createTitleMarkup(block);
+            break;
+        case "text":
+            html = createTextMarkup(block);
+            break;
+        case "columns":
+            html = createColumnsMarkup(block);
+            break;
+        default:
+            html = ``;
+    }
+
+    document.querySelector('#site').insertAdjacentHTML(`beforeend`, html);
+})
