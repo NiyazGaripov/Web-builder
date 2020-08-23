002bb3e99e2b6a11e0925480ec1a2768871d7977
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-23 19:41:30 +0300

    Adds styles to options

--- src/model.js
+++ src/model.js
@@ -1,5 +1,11 @@
 export const models = [
-    {type: `title`, value: `Web Builder`, options: {}},
-    {type: `text`, value: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur error expedita hic itaque maiores minus nulla numquam perferendis placeat sapiente!`, options: {}},
-    {type: `columns`, value: [`first column`, `second column`, `third column`], options: {}},
+    {type: `title`, value: `Web Builder`, options: {
+        styles: `color: white; text-align: center; background-color: green;`
+    }},
+    {type: `text`, value: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur error expedita hic itaque maiores minus nulla numquam perferendis placeat sapiente!`, options: {
+        styles: `padding: 10px;`
+    }},
+    {type: `columns`, value: [`first column`, `second column`, `third column`], options: {
+        styles: `background-color: coral; text-align: center;`
+    }},
 ];
