c6b5b475df331e995fe31066e03fe9f763cc165c
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-23 18:27:50 +0300

    Imports functions

--- src/index.js
+++ src/index.js
@@ -1,37 +1,8 @@
 import {models} from './model';
 import './styles/main.css'
+import {createColumnsMarkup, createTextMarkup, createTitleMarkup} from "./templates";
 
 const site = document.querySelector('#site');
-
-const createTitleMarkup = (block) => {
-    return (
-        `<div class="row">
-            <div class="col-sm">
-                <h1>${block.value}</h1>
-            </div>
-        </div>`
-    );
-};
-
-const createTextMarkup = (block) => {
-    return (
-        `<div class="row">
-            <div class="col-sm">
-                <p>${block.value}</p>
-            </div>
-        </div>`
-    );
-};
-
-const createColumnsMarkup = (block) => {
-    const column = block.value.map(item => `<div class="col-sm"><p>${item}</p></div>`).join('');
-
-    return (
-        `<div class="row">
-            ${column}
-        </div>`
-    );
-};
 
 models.forEach(block => {
     let html;
