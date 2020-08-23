bf9a433ce3267e6a49578cd3f60a4563a3c630ea
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-23 19:43:36 +0300

    Adds styles

--- src/templates.js
+++ src/templates.js
@@ -3,21 +3,19 @@ import {getCol, getRow} from "./utils";
 const createTitleMarkup = (block) => {
     return getRow(getCol(
         `<h1>${block.value}</h1>`
-        )
-    );
+    ), block.options.styles);
 };
 
 const createTextMarkup = (block) => {
     return getRow(getCol(
         `<p>${block.value}</p>`
-        )
-    );
+    ), block.options.styles);
 };
 
 const createColumnsMarkup = (block) => {
     const column = block.value.map(item => getCol(`<p>${item}</p>`)).join('');
 
-    return getRow(column);
+    return getRow(column, block.options.styles);
 };
 
 export const templates = {createTitleMarkup, createTextMarkup, createColumnsMarkup}
