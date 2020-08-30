6684ae52fa52dfd54935fe66fe731f1d8311a512
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-30 16:24:17 +0300

    Corrects model data

--- src/model.js
+++ src/model.js
@@ -2,18 +2,18 @@ import img from './img/logo.svg';
 
 export const models = [
     {type: `navigation`, value: [`Tours`, `Training`, `About us`, `Contacts`], options: {
-        styles: `padding: 5px;`,
+        styles: `padding: 5px 30px;`,
         img: {
             path: `${img}`,
             alt: 'Logo company'
         }
     }},
-    {type: `title`, value: `Travel around the world`, options: {
-        tag: `h2`,
-        styles: `color: white; text-align: center; background-color: green;`
+    {type: `title`, value: `Hiking in the mountains of Russia`, options: {
+        tag: `h1`,
+        styles: `margin-bottom: 20px; padding-top: 150px; font-size: 45px; font-weight: bold; color: white;`
     }},
-    {type: `text`, value: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur error expedita hic itaque maiores minus nulla numquam perferendis placeat sapiente!`, options: {
-        styles: `padding: 10px;`
+    {type: `text`, value: `We organize excursions professionally to the most beautiful peaks of the planet`, options: {
+        styles: `padding-bottom: 340px; font-size: 20px; color: white`
     }},
     {type: `columns`, value: [`first column`, `second column`, `third column`], options: {
         styles: `background-color: coral; text-align: center;`
