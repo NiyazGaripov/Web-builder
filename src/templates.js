69121774f9b5dd1f8300cf77159e53b0fcc37705
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-30 19:13:41 +0300

    Creates func createColumnMarkup

--- src/templates.js
+++ src/templates.js
@@ -28,8 +28,19 @@ const createTextMarkup = (block) => {
     ), block.options.styles);
 };
 
+const createColumnMarkup = (item) => {
+    const {name, img} = item;
+
+    return (
+        `<a href="" class="card">
+            <h3 class="card__title">${name}</h3>
+            <img src="${img}" alt="${name}">
+        </a>`
+    );
+}
+
 const createColumnsMarkup = (block) => {
-    const column = block.value.map(item => getCol(`<p>${item}</p>`)).join('');
+    const column = block.value.map(item => getCol(createColumnMarkup(item))).join('');
 
     return getRow(column, block.options.styles);
 };
