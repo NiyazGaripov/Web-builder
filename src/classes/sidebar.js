01be61ec86f54696f9050cbd2a6ee59c050844b8
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-31 21:46:39 +0300

    Creates method init

--- src/classes/sidebar.js
+++ src/classes/sidebar.js
@@ -1,5 +1,11 @@
 export class Sidebar {
     constructor(selector) {
         this.$el = document.querySelector(selector);
+
+        this.init();
+    }
+
+    init() {
+        this.$el.innerHTML = `<h3>Test</h3>`
     }
 }
