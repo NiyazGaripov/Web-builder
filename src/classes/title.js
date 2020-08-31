1bef8132a731448f12b58fe4b44fb81c98d21f7d
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-31 17:54:37 +0300

    Replaces func createTitleMarkup from module templates

--- src/classes/title.js
+++ src/classes/title.js
@@ -1,4 +1,13 @@
+import {getCol, getRow} from "../utils";
 import {Block} from './block';
+
+const createTitleMarkup = (block) => {
+    const {tag, styles} = block.options;
+
+    return getRow(getCol(
+        `<${tag}>${block.value}</${tag}>`
+    ), styles);
+};
 
 export class Title extends Block {
     constructor(value, options) {
