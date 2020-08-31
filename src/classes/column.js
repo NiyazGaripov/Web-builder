11da50dd8bb3616b9ca0908357d75f7b4126f0f3
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-31 18:08:34 +0300

    Transfers func createColumnsMarkup from module templates

--- src/classes/column.js
+++ src/classes/column.js
@@ -1,4 +1,22 @@
+import {getCol, getRow} from "../utils";
 import {Block} from "./block";
+
+const createColumnMarkup = (item) => {
+    const {name, img} = item;
+
+    return (
+        `<a href="" class="card">
+            <h3 class="card__title">${name}</h3>
+            <img src="${img}" alt="${name}">
+        </a>`
+    );
+}
+
+const createColumnsMarkup = (block) => {
+    const column = block.value.map(item => getCol(createColumnMarkup(item))).join('');
+
+    return getRow(column, block.options.styles);
+};
 
 export class Column extends Block {
     constructor(value, options) {
