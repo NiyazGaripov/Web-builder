0edd017f7193db66f674ba50765622eb4d6e9601
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-31 18:03:43 +0300

    Transfers func createTextMarkup from module templates

--- src/classes/text.js
+++ src/classes/text.js
@@ -1,4 +1,11 @@
+import {getCol, getRow} from "../utils";
 import {Block} from "./block";
+
+const createTextMarkup = (block) => {
+    return getRow(getCol(
+        `<p style="margin-bottom: 0">${block.value}</p>`
+    ), block.options.styles);
+};
 
 export class Text extends Block {
     constructor(value, options) {
