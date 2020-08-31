917878ba079634a734372b18127838cc6a7a5529
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-31 21:11:49 +0300

    Refactors module index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import {models} from './model';
+import {Site} from "./classes/site";
 import './styles/main.css'
 
-const site = document.querySelector('#site');
-
-models.forEach(block => {
-    site.insertAdjacentHTML(`beforeend`, block.getTemplate());
-})
+const site = new Site('#site');
+site.render(models);
