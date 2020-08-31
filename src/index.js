3ff6923cf5546d4f274c8b5104c913748d01ad73
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-31 18:26:16 +0300

    Removes unused code and calls method for each instance

--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
 import {models} from './model';
 import './styles/main.css'
-import {templates} from "./templates";
 
 const site = document.querySelector('#site');
 
 models.forEach(block => {
-    let html;
-
-    switch (block.type) {
-        case "header":
-            html = templates.createHeaderMarkup(block);
-            break;
-        case "title":
-            html = templates.createTitleMarkup(block);
-            break;
-        case "text":
-            html = templates.createTextMarkup(block);
-            break;
-        case "columns":
-            html = templates.createColumnsMarkup(block);
-            break;
-        default:
-            html = ``;
-    }
-
-    site.insertAdjacentHTML(`beforeend`, html);
+    site.insertAdjacentHTML(`beforeend`, block.getTemplate());
 })
