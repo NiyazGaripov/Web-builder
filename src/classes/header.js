7d6e6ab03666bf53c8e0b25d5d38d0de291aed92
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-31 17:45:59 +0300

    Replaces func createHeaderMarkup from module templates

--- src/classes/header.js
+++ src/classes/header.js
@@ -1,4 +1,21 @@
 import {Block} from './block';
+import {getCol} from "../utils";
+
+const createHeaderMarkup = (block) => {
+    const {img, styles} = block.options;
+    const path = block.value.map(it => `${it.charAt(0).toLowerCase()}${it.slice(1)}`);
+    const item = block.value.map((it, index) => `<a href="${path[index]}" style="${styles}">${it}</a>`).join('\n');
+
+    return getCol(
+        `<header class="header">
+            <img src="${img.path}" alt="${img.alt}">
+            <nav>
+                ${item}
+            </nav>
+        </header>`
+    );
+};
+
 
 export class Header extends Block {
     constructor(value, options) {
