ce7d155779d293bfbfa608c27a17fa43a8105139
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-31 21:11:25 +0300

    Creates method render

--- src/classes/site.js
+++ src/classes/site.js
@@ -2,4 +2,10 @@ export class Site {
     constructor(selector) {
         this.$el = document.querySelector(selector);
     }
+
+    render(models) {
+        models.forEach(model => {
+            this.$el.insertAdjacentHTML(`beforeend`, model.getTemplate());
+        })
+    }
 }
