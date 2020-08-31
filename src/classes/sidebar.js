d416c28844e4b5020538af1d67827bdfe106ecc4
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-08-31 21:56:28 +0300

    Creates func createForm

--- src/classes/sidebar.js
+++ src/classes/sidebar.js
@@ -1,3 +1,19 @@
+const createForm = (type) => {
+    return (
+        `<form name="${type}">
+            <h5>${type}</h5>
+            <div class="form-group">
+                <input class="form-control form-control-sm" name="value" placeholder="value">
+            </div>
+            <div class="form-group">
+                <input class="form-control form-control-sm" name="styles" placeholder="styles">
+            </div>
+            <button type="submit" class="btn btn-primary btn-sm">Create</button>
+        </form>
+        <hr />`
+    );
+}
+
 export class Sidebar {
     constructor(selector) {
         this.$el = document.querySelector(selector);
