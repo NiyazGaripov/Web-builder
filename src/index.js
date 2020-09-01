64e0e45d09ca1ee5547cd6c40740071f5d9c7933
Niyaz Garipov <Niayz_Indicus@mail.ru>
2020-09-01 12:23:04 +0300

    Creates func update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import {models} from './model';
+import {Sidebar} from "./classes/sidebar";
 import {Site} from "./classes/site";
 import './styles/main.css'
 
 const site = new Site('#site');
 site.render(models);
+
+const update = (newBlock) => {
+    models.push(newBlock);
+    site.render(models);
+}
+
+new Sidebar('#panel', update);
